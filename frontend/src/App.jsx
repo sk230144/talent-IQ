66546349b8b4d9d5f3cb2229b8ea2d4e73aa8a4d
Saurabh <risabht043@gmail.com>
2025-11-08 16:53:00 +0530

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react';
-import './App.css'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <h1>Welcome to app</h1>
