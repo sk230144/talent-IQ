f01957a2264180b5ee1f11733d8a9229744194e6
Saurabh <risabht043@gmail.com>
2025-11-17 21:27:55 +0530

    fix

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,4 +1,5 @@
 import { StreamChat } from 'stream-chat'
+import { StreamClient } from '@stream-io/node-sdk'
 import { ENV } from './env.js'
 
 
@@ -12,7 +13,8 @@ if (!apiKey || !apiSecret) {
 }
 
 
-export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // will be used chat features
+export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
 
 
 export const upsertStreamUser = async (userData) => {
