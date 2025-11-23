12f91ac7c4c14908a069d815cdac520a4d88ab52
Saurabh <risabht043@gmail.com>
2025-11-23 18:03:16 +0530

    make landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/clerk-react';
 import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import DashboardPage from './pages/DashboardPage';
@@ -9,7 +9,9 @@ import { Toaster } from 'react-hot-toast';
 
 function App() {
 
-  const isSignedIn = true;
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) return null;
 
   return (
     <>
